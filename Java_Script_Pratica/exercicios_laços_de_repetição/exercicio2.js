f94ffc1bc5f1e266c16512923646a3a83e65f8b6
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-04-20 12:10:52 -0300

    exercicio 2 laços de repetição

---
+++ Java_Script_Pratica/exercicios_laços_de_repetição/exercicio2.js
@@ -0,0 +1,23 @@
+// Você é uma pessoa desenvolvedora de interface em uma startup de tecnologia aeroespacial e precisa criar uma contagem regressiva automática para o painel de lançamento. A contagem deve começar de 10 até 0, exibindo “Lançar!” ao final.
+
+// Crie um programa que conte de 10 até 0 e exiba a mensagem “Lançar!” ao final.
+
+// Saída esperada:
+
+// 10  
+// 9  
+// 8  
+// 7  
+// 6  
+// 5  
+// 4  
+// 3  
+// 2  
+// 1  
+// 0  
+// Lançar!
+const NumeroIncicial = 50
+for (c=NumeroIncicial;c >= 0; c--){
+    console.log(c)
+}
+console.log("Lançar !")
