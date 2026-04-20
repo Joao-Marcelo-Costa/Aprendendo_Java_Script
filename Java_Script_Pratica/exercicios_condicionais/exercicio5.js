7d05b72f2ac17cebe1e8127bec3bc317603f8645
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-04-20 11:32:30 -0300

    exercicio 5

---
+++ Java_Script_Pratica/exercicios_condicionais/exercicio5.js
@@ -0,0 +1,20 @@
+// Você é uma pessoa desenvolvedora de um sistema para uma escola que organiza atividades para diferentes faixas etárias. As regras são:
+// Se a pessoa tiver menos de 12 anos, ela deve ser classificada como "Infantil".
+// Se a pessoa tiver 12 anos ou mais, mas menos de 18 anos, ela deve ser classificada como "Juvenil".
+// Se a pessoa tiver 18 anos ou mais, ela deve ser classificada como "Adulto".
+// Crie um programa que, dado o valor da idade, classifique a pessoa na categoria correspondente.
+// Exemplo de entrada:
+// const idade = 15;
+// Saída esperada:
+// Se idade for 8:
+// Infantil
+// Se idade for 13:
+// Juvenil
+// Se idade for 20:
+// Adulto
+const IdadeDoAtleta = 18
+if (IdadeDoAtleta ){
+    if (IdadeDoAtleta < 12){ console.log("O atleta é infantil")}
+    else if (IdadeDoAtleta <= 18){console.log("O atleta é juvenil")}
+    else {console.log("O atleta é adulto")}
+}else{console.log("Idade não informada devidademente")}
