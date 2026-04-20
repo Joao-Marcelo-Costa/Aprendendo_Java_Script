20f71b65da940ed0fd801b57939d3217a66c53d4
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-04-20 12:08:22 -0300

    exercicio 1 laços de repetição

---
+++ Java_Script_Pratica/exercicios_laços_de_repetição/exercicio1.js
@@ -0,0 +1,18 @@
+// Você está criando um aplicativo para exibir a sequência de números até um valor que o usuário forneceu. O objetivo é mostrar todos os números a partir de 1 até o valor informado pelo usuário, de forma ordenada e crescente.
+
+// Como você pode gerar essa contagem e garantir que cada número seja mostrado até atingir o valor final fornecido?
+
+// Exemplo de entrada:
+
+// const numeroFinal = 5;
+// Saída esperada:
+// 1
+// 2
+// 3
+// 4
+// 5
+
+const InformarAte = 10
+for ( c=1; c <= InformarAte; c++){
+    console.log(c)
+}
