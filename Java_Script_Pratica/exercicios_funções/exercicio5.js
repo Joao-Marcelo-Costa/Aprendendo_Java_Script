ce667b91909e2b68219c5107897946f7392c5f0d
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-04-21 22:51:24 -0300

    exercicio 5 funções

---
+++ Java_Script_Pratica/exercicios_funções/exercicio5.js
@@ -0,0 +1,18 @@
+// Imagine que você está desenvolvendo uma interface de atendimento online.Quando o usuário envia uma pergunta, o sistema precisa simular que está "pensando" ou "processando a resposta", e só depois exibir a resposta final.
+
+// Sua tarefa é criar uma função chamada responderUsuario, que receba dois parâmetros:
+
+// o nome do usuário, e
+// uma função de callback que será executada após 3 segundos.
+// Exemplo de entrada:
+
+// responderUsuario("Camila", mostrarResposta);
+// Saída esperada (com 3 segundos de atraso):
+
+// Processando sua pergunta...
+// Olá, Camila! Aqui está a resposta para sua dúvida.
+function responderUsuario (nomeDeUsuário,mensagem){
+    console.log("Processando sua pergunta...")
+    setTimeout((mensagem)=>{console.log( "Olá", nomeDeUsuário,"aqui está a resposta para a sua duvida")},3000)
+}
+responderUsuario("ana")
