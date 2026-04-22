02c5925f791d0af2a16ff1f1296d9a7847680e35
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-04-21 22:42:29 -0300

    exercicio 4 funções

---
+++ Java_Script_Pratica/exercicios_funções/exercicio4.js
@@ -0,0 +1,15 @@
+// Imagine que você está criando uma pequena ferramenta para alunos acompanharem seu desempenho escolar. A ideia é que o aluno insira suas duas notas principais e o sistema exiba a média aritmética entre elas.
+
+// Sua tarefa é criar uma arrow function chamada calcularMedia que receba duas notas e retorne a média entre elas.
+
+// Exemplo de entrada:
+
+// calcularMedia(7, 9);
+// Copiar código
+// Saída esperada:
+
+// 8
+function media(nota1, nota2){
+    console.log((nota1 + nota2)/2)
+}
+media(1,2)
