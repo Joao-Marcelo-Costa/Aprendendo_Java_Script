6a9d830abe50995c8da63f5ef073d60abbf08ad8
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-04-29 15:03:56 -0300

    exercicio 3 objetos

---
+++ Java_Script_Pratica/exercicios_objetos/exercicio3.js
@@ -0,0 +1,20 @@
+// Você está criando um aplicativo de culinária que mostra receitas com seus ingredientes, tempo de preparo e instruções. Cada receita é representada por um objeto que contém uma lista de ingredientes armazenada como um array. Diante disso, escreva um programa que:
+
+// Crie um objeto receita com as propriedades: nome, ingredientes (array com pelo menos 5 itens) e tempoPreparo.
+// Acesse o índice 2 da lista que está dentro do objeto.
+// Exiba no console a frase: "Ingrediente complementar: [ingrediente]".
+// Exemplo de entrada:
+
+// const receita = {
+//   // preencha com as propriedades necessárias
+// };
+// Saída esperada:
+
+// Ingrediente complementar: farinha de trigo
+const receita = {
+    nome: "bolo de mandioca",
+    ingredientes:["Mandioca","Farinha de trigo","Ovo","Leite","Açúcar"],
+    tempoPreparo: 120//120 minutos
+
+}
+console.log(`Ingrediente complementar ${receita.ingredientes[2]}`)
